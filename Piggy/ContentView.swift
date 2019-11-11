86e6ef78ce6c7420e695f62a3b61f3abfdbf689c
Taylor Bauer <taylorbauer@ku.edu>
2019-11-11 10:18:36 -0600

    Initial Commit

---
+++ Piggy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Piggy
+//
+//  Created by Taylor Bauer on 11/11/19.
+//  Copyright © 2019 Group 6. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
