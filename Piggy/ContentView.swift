959ca88903f1b1b7889fd9fe2bcd47a82ea9d24e
Taylor Bauer <taylorbauer@ku.edu>
2019-11-11 10:53:11 -0600

    Ugly skeleton of main menu

--- Piggy/ContentView.swift
+++ Piggy/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack (alignment: .leading) {
+            HStack {
+                Text ("Welcome to Piggy")
+                    .font(.title)
+                    .padding()
+                Spacer()
+                PiggyLogo()
+                    .offset(x: -10, y: -10)
+            }
+            Text("Main Menu:")
+                .padding()
+            List {      // To be converted to NavigationView when ready
+                Text("Inventory")
+                Text("Recipes")
+                Text("Business Analytics")
+            }
+ 
+            Spacer()
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPad Pro (10.5-inch)"))
     }
 }
---
+++ Piggy/PiggyLogo.swift
@@ -0,0 +1,21 @@
+//
+//  PiggyLogo.swift
+//  Piggy
+//
+//  Created by Taylor Bauer on 11/11/19.
+//  Copyright © 2019 Group 6. All rights reserved.
+//
+
+import SwiftUI
+
+struct PiggyLogo: View {
+    var body: some View {
+        Image("piggy_logo_concept")
+    }
+}
+
+struct PiggyLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        PiggyLogo()
+    }
+}
