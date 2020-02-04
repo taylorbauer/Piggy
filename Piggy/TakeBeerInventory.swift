43f7afaf857b279272129bf82fdc5a06b93b9a8d
Taylor Bauer <taylorbauer@ku.edu>
2020-02-04 10:51:17 -0600

    Added beer inventory and associated .json

--- Piggy/TakeBeerInventory.swift
+++ Piggy/TakeBeerInventory.swift
@@ -1,29 +1,19 @@
 //
-//  TakeLiquorInventory.swift
+//  TakeBeerInventory.swift
 //  Piggy
 //
-//  Created by Taylor Bauer on 11/22/19.
-//  Copyright © 2019 Group 6. All rights reserved.
+//  Created by Taylor Bauer on 2/4/20.
+//  Copyright © 2020 Group 6. All rights reserved.
 //
-
-//  This is the page that will contain all the actual tools for
-//  recording inventory.  It is called on by NewInventoryPage.
-
-//  Right now it doesn't store the data and it is not very pretty.
-//  It is more a proof of concept for once we actually decide on a
-//  scheme.
-
-//  There is an issue generating as many pickerwheels as this menu
-//  currently generates.  If this list gets long enough it eventually
-//  crashes.
+//  For comments on how this view works, please view
+//  "TakeLiquorInventory.swift"
+//
 
 import SwiftUI
 
-struct TakeLiquorInventory: View {
-    
-    // Decode inventory.json to create InventoryItem objects
-    // This list of objects is called "items"
-    let items = Bundle.main.decode([InventoryItem].self, from: "liquor_inventory.json")
+struct TakeBeerInventory: View {
+
+    let items = Bundle.main.decode([InventoryItem].self, from: "beer_inventory.json")
 
     var body: some View {
         VStack() {
@@ -34,20 +24,14 @@ struct TakeLiquorInventory: View {
                 .padding(.leading)
             
             List {
-                // Here, "bottle" is each instance of class InventoryItem
-                // Now, "items" is a list of "bottle"s that we use to generate the views
+
                 ForEach(items) { bottle in
                     VStack(alignment: .leading) {
                         Text(bottle.name)
                             .font(.headline)
                         HStack {
                             
-                            // InventoryStepper is the view that contains the
-                            // actual counter for the items.
-                            // We will pull the result out of the "current"
-                            // variable in this view when we implement our database
                             InventoryStepper(itemType: bottle)
-                            // This is a little ugly and we can pretty it up later
 
                             Spacer()
                         }
@@ -60,7 +44,7 @@ struct TakeLiquorInventory: View {
                     Button(action: {
                         print("Inventory submitted (not really)")
                     }) {
-                        Text("SUBMIT LIQUOR INVENTORY")
+                        Text("SUBMIT BEER INVENTORY")
                             .fontWeight(.bold)
                             .font(.title)
                             .padding()
@@ -82,11 +66,9 @@ struct TakeLiquorInventory: View {
                 }
             }
 }
-            
 
-struct TakeLiquorInventory_Previews: PreviewProvider {
+struct TakeBeerInventory_Previews: PreviewProvider {
     static var previews: some View {
-        TakeLiquorInventory()
-        //.previewDevice(PreviewDevice(rawValue: "iPad Pro (10.5-inch)"))
+        TakeBeerInventory()
     }
 }
