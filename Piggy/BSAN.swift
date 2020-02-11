d13490963d4c4a8b042bda21ab885189529e5e9f
Mitch Simmons <mitch.t.simmons@gmail.com>
2020-02-11 10:37:31 -0600

    *DATABASE IN BSAN* ~ attempted to add the main database to the BSAN.swift file, as I am committing I am having second thoughts on where the database should be... maybe in InventoryView instead??

--- Piggy/BSAN.swift
+++ Piggy/BSAN.swift
@@ -9,11 +9,54 @@
 // This will be the main landing page for business analytics
 
 import SwiftUI
+import SQLite3
 
 struct BSAN: View {
     var body: some View {
         VStack {
             Text("This is the main menu for business analytics")
+			/*
+				// adding database
+				
+				var db: OpaquePointer?
+				
+				let fileURL = FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendPathComponent("PiggyDatabase.sqlite")
+				
+				if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+					print("Error Opening Database")
+				}
+				
+				let createTableQuery = """
+					CREATE TABLE IF NOT EXISTS
+									Alcohol
+					(id INTEGER PRIMARY KEY AUTOINCREMENT,
+					name TEXT,
+					drink_group_id INTEGER,
+					inventory INTEGER,
+					CUR_PRICE DOUBLE
+					PREV_PRICE
+					COGS DOUBLE,
+					INV_1_PRIOR INT,
+					INV_1_PRIOR_DATE DATE,
+					INV_2_PRIOR INT,
+					INV_2_PRIOR_DATE DATE,
+					INV_3_PRIOR INT,
+					INV_3_PRIOR_DATE DATE,
+					INV_4_PRIOR INT,
+					INV_4_PRIOR_DATE DATE,
+					INV_5_PRIOR INT,
+					INV_5_PRIOR_DATE DATE,
+					Demand_Coefficient DOUBLE,
+					RunDown_Time DOUBLE)
+										"""
+				
+				sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK {
+					print("Error Creating Table")
+				}
+				
+				print("Everything is Fine")
+			
+			*/
             Spacer()
         }
     }
