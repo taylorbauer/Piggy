40b0a4b91ffc7592174457e9740c685399fc71d1
Taylor Bauer <taylorbauer@ku.edu>
2020-04-27 12:35:32 -0500

    Added category attribute to database and associated picker to menus

--- Piggy/AddInventoryItem.swift
+++ Piggy/AddInventoryItem.swift
@@ -14,6 +14,8 @@ struct AddInventoryItem: View {
     @State private var par: String = ""
     @State private var initialCount: String = ""
     @State private var showingAlert = false
+    @State private var category = ""
+    var categories = ["Liquor", "Wine", "Beer"]
     
     @Environment(\.managedObjectContext) var managedObjectContext
     @FetchRequest(entity: InventoryDBItem.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \InventoryDBItem.name, ascending: true)]) var items: FetchedResults<InventoryDBItem>
@@ -27,62 +29,72 @@ struct AddInventoryItem: View {
                     .padding()
                 Group{
                     Group{
+                        Picker(selection: $category, label: Text("Type").bold()) {
+                            ForEach(0 ..< categories.count) {
+                                Text(self.categories[$0])
+                            }
+                        }
+                        
+                        
                         Text("Item Name")
                             .font(.headline)
                         
                         TextField("Name", text: $name)
                             .padding()
                             .background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 0.3))
-                        Text("Price")
-                            .font(.headline)
-                        
-                        
-                        TextField("Price", text: $price)
-                            .padding()
-                            .background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 0.3))
+                            
+                            Text("Price")
+                                .font(.headline)
+                            
+                            
+                            TextField("Price", text: $price)
+                                .padding()
+                                .background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 0.3))
+                        }
+                        Group {
+                            Text("Par")
+                                .font(.headline)
+                            TextField("Par", text: $par)
+                                .padding()
+                                .background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 0.3))
+                            Text("initial Count")
+                                .font(.headline)
+                            TextField("initial Count", text: $initialCount)
+                                .padding()
+                                .background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 0.3))
+                        }
                     }
-                    Group {
-                        Text("Par")
-                            .font(.headline)
-                        TextField("Par", text: $par)
-                            .padding()
-                            .background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 0.3))
-                        Text("initial Count")
-                            .font(.headline)
-                        TextField("initial Count", text: $initialCount)
-                            .padding()
-                            .background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 0.3))
-                    }
-                }
-                
-                NavigationLink(destination: NewInventoryPage()) {
-                    Button("Add") {
-                        print("Successfully added to database")
-                        let inventorySample = InventoryDBItem(context: self.managedObjectContext)
-                        inventorySample.id = UUID()
-                        inventorySample.name = self.name
-                        inventorySample.currentCount = Int16(self.initialCount)!
-                        inventorySample.previousCount = 0
-                        inventorySample.par = Int16(self.par)!
-                        inventorySample.price = Double(self.price)!
-                        
-                        try? self.managedObjectContext.save()
-                        
-                        self.showingAlert = true
-                        
-                        self.name = ""
-                        self.initialCount = ""
-                        self.par = ""
-                        self.price = ""
-                    }
-                    .alert(isPresented: $showingAlert) {
-                        Alert(title: Text("Success!"), message: Text("Successfully saved new item to database"), dismissButton: .default(Text("OK")))
-                    }
-                    .font(.title).padding().foregroundColor(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 1.0))
-                }.padding()
+                    
+                    NavigationLink(destination: NewInventoryPage()) {
+                        Button("Add") {
+                            print("Successfully added to database")
+                            let inventorySample = InventoryDBItem(context: self.managedObjectContext)
+                            inventorySample.id = UUID()
+                            inventorySample.name = self.name
+                            inventorySample.currentCount = Int16(self.initialCount)!
+                            inventorySample.previousCount = 0
+                            inventorySample.par = Int16(self.par)!
+                            inventorySample.price = Double(self.price)!
+                            inventorySample.category = self.category
+                            
+                            try? self.managedObjectContext.save()
+                            
+                            self.showingAlert = true
+                            
+                            self.name = ""
+                            self.initialCount = ""
+                            self.par = ""
+                            self.price = ""
+                            self.category = ""
+                        }
+                        .alert(isPresented: $showingAlert) {
+                            Alert(title: Text("Success!"), message: Text("Successfully saved new item to database"), dismissButton: .default(Text("OK")))
+                        }
+                        .font(.title).padding().background(Color(red: 0.88, green: 0.65, blue: 0.86, opacity: 1.0)).foregroundColor(.white)
+                    }.padding()
+            }.padding()
             }
         }
-    }
         
         struct AddInventoryItem_Previews: PreviewProvider {
             static var previews: some View {
